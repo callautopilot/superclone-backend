6bfc524605d0858887b342904c579c23d9ef60a3
Louis <dev@louis.io>
2024-01-16 12:45:51 +0100

    Working backend transcription from audio stream

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,60 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
+const sdk_1 = require("@deepgram/sdk");
+const dotenv_1 = __importDefault(require("dotenv"));
+const ws_1 = require("ws");
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT;
+const wss = new ws_1.WebSocket.Server({ port: 1338 });
+const deepgramClient = (0, sdk_1.createClient)(process.env.DEEPGRAM_API_KEY);
+const url = "https://icecast.radiofrance.fr/franceinter-midfi.mp3";
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html'); // Serve your client webpage here
+});
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    const connection = deepgramClient.listen.live({
+        punctuate: true,
+        smart_format: true,
+        model: "nova",
+    });
+    let keepAlive = setInterval(() => {
+        console.log("deepgram: keepalive");
+        connection.keepAlive();
+    }, 10 * 1000);
+    connection.on(sdk_1.LiveTranscriptionEvents.Open, () => {
+        connection.on(sdk_1.LiveTranscriptionEvents.Close, () => {
+            console.log("Connection closed.");
+        });
+        connection.on(sdk_1.LiveTranscriptionEvents.Metadata, (data) => {
+            console.log(data);
+        });
+        connection.on(sdk_1.LiveTranscriptionEvents.Transcript, (data) => {
+            console.log(data);
+        });
+        (0, node_fetch_1.default)(url)
+            .then(response => {
+            const stream = response.body;
+            stream.on("readable", () => {
+                const chunk = stream.read();
+                if (chunk) {
+                    connection.send(chunk);
+                }
+            });
+        })
+            .catch(error => {
+            console.error('Error fetching audio stream:', error);
+        });
+    });
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        clearInterval(keepAlive);
+        if (connection)
+            connection.finish();
+    });
 });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,74 @@
 import express from 'express';
+import fetch from "node-fetch";
+import { createClient, LiveTranscriptionEvents } from '@deepgram/sdk';
+import dotenv from 'dotenv';
+import { WebSocket } from 'ws';
+
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const wss = new WebSocket.Server({ port: 1338 });
+
+const deepgramClient = createClient(process.env.DEEPGRAM_API_KEY);
+// const url = "https://stream.live.vc.bbcmedia.co.uk/bbc_world_service_americas";
+const url = "https://icecast.radiofrance.fr/franceinter-midfi.mp3";
 
 app.get('/', (req, res) => {
-  res.send('🤖 Superclone AI');
+  res.sendFile(__dirname + '/index.html'); // Serve your client webpage here
+});
+
+wss.on('connection', (ws: WebSocket) => {
+  console.log('Client connected');
+
+  const connection = deepgramClient.listen.live({
+    punctuate: false,
+    smart_format: false,
+    model: "nova-2",
+    language: "fr",
+  });
+
+  let keepAlive = setInterval(() => {
+    // console.log("deepgram: keepalive");
+    connection.keepAlive();
+  }, 10 * 1000);
+
+  connection.on(LiveTranscriptionEvents.Open, () => {
+    connection.on(LiveTranscriptionEvents.Close, () => {
+      console.log("Connection closed.");
+      clearInterval(keepAlive);
+      if (connection) connection.finish();
+    });
+
+    connection.on(LiveTranscriptionEvents.Metadata, (data) => {
+      console.log(data);
+    });
+
+    connection.on(LiveTranscriptionEvents.Transcript, (data) => {
+      const transcript = data.channel.alternatives[0].transcript
+      console.log(transcript);
+      ws.send(JSON.stringify(data));
+    });
+
+    fetch(url)
+      .then(response => {
+        const stream = response.body;
+        stream.on("readable", () => {
+          const chunk = stream.read();
+          if (chunk) {
+            connection.send(chunk);
+          }
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching audio stream:', error);
+      });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    clearInterval(keepAlive);
+    if (connection) connection.finish();
+  });
 });
 
 app.listen(port, () => {
